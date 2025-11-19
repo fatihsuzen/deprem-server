f791d0f9e93ed3ba1b81d2e990409e28d83b18c1
fatihsuzen <53154457+fatihsuzen@users.noreply.github.com>
2025-11-19 16:38:28 +0300

    server klasörü ve içeriği tamamen silindi

--- server/models/UserToken.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const UserTokenSchema = new mongoose.Schema({
-  userId: String,
-  token: String,
-  platform: String,
-  updatedAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('UserToken', UserTokenSchema);
