824eff5f18996758c80177c136649740c75cd6ea
fatihsuzen <53154457+fatihsuzen@users.noreply.github.com>
2025-11-19 16:57:48 +0300

    send-push-all.bat dosyası /fcm/send-all endpointine güncellendi

--- send-push-all.bat
+++ send-push-all.bat
@@ -5,6 +5,6 @@ setlocal
 set TITLE=Deprem Uyarısı
 set BODY=Bölgenizde deprem algılandı!
 
-curl -X POST http://188.132.202.24:3000/send-all -H "Content-Type: application/json" -d "{\"title\":\"%TITLE%\",\"body\":\"%BODY%\",\"data\":{\"type\":\"earthquake\"}}"
+curl -X POST http://188.132.202.24:3000/fcm/send-all -H "Content-Type: application/json" -d "{\"title\":\"%TITLE%\",\"body\":\"%BODY%\",\"data\":{\"type\":\"earthquake\"}}"
 endlocal
 pause
