5661c9eea02b341e7e1df70c4c13cd23c7e248df
fatihsuzen <53154457+fatihsuzen@users.noreply.github.com>
2025-11-19 04:24:19 +0300

    Ana dizinde FCM endpointleri doğrudan çalışacak şekilde router güncellendi

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.use('/api/friends', friendsRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/earthquakes', earthquakesRoutes);
 app.use('/api/settings', settingsRoutes);
-app.use('/api/fcm', fcmRoutes);
+  app.use(fcmRoutes);
 
 // User routes for location updates
 app.post('/api/users/update-location', async (req, res) => {
